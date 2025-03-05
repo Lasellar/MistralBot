19480b25b1cf3030c09786ef57182ad4acc01fed
Maksim Polushin <btnchsky.sad@mail.ru>
2025-03-05 17:45:03 +0300

    last fixes

--- main.py
+++ main.py
@@ -15,20 +15,17 @@ client = Mistral(api_key=MISTRAL_TOKEN)
 @bot.on_message()
 async def echo(_, message: Message):
     try:
-        if (
-            message.text.lower().startswith('бот,')
-        ):
-            temp_message = await bot.send_message(chat_id=message.chat.id, text='думаю...')
-            response = gpt_4o_response(client, message.text)
-            await bot.edit_message_text(
-                chat_id=temp_message.chat.id,
-                message_id=temp_message.id,
-                text=response
-            )
+        temp_message = await bot.send_message(chat_id=message.chat.id, text='думаю...')
+        response = gpt_4o_response(client, message.text)
+        await bot.edit_message_text(
+            chat_id=temp_message.chat.id,
+            message_id=temp_message.id,
+            text=response
+        )
     except AttributeError:
         pass
     except SDKError:
-        await bot.send_message(chat_id=message.chat.id, text='Лимит запросов за час блять')
+        await bot.send_message(chat_id=message.chat.id, text='Лимит запросов за час')
 
 while True:
     print('Запущен')
